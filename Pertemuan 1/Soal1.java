e8f2474eb701726d69eb2330908f4ff2c9438abc
Hanif Ahmad Naufal <153438391+ramenyeon@users.noreply.github.com>
2024-08-22 20:23:51 +0700

    Pertemuan 1

---
+++ Pertemuan 1/Soal1.java
@@ -0,0 +1,18 @@
+/*
+	Nama        : Hanif Ahmad Naufal
+	NIM         : 231511048
+	Kelas       : D3-2B
+	Mata Kuliah : Pemrograman Berorientasi Objek
+	Tanggal     : 22 Agustus 2024
+ */
+
+
+public class Soal1 {
+    public static void main (String[] args) {
+        byte angka1 = 125;
+        byte angka2 = 6;
+        byte hasil = (byte) (angka1 + angka2);
+
+        System.out.println("Hasil 1 " + hasil);
+    }
+}
